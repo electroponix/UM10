8191f3b0d9c273c26e9ae6945601e6a62a290c38
Jeff Wiegand <electroponix@gmail.com>
2022-10-29 17:48:10 -0500

    after database export in case I have to come back to any config files.
    I am going to load it on Suse box. Run the DB local and alter config files? Docker export? It runs now.

---
+++ sites/default/files/php/twig/635d81b1c5ce4___string_template__249a3a_cjYZI6NBbyPInoSSHg0KLWwzj/XnQILvQ2RGymQn5a0OBFhsolXnpnLEklsY3i-msVqoI.php
@@ -0,0 +1,84 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* __string_template__249a3a63d9c751b1b9c960794fcedf18 */
+class __TwigTemplate_f56e50ff331d25a4c67ae0e839feda9b extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo "destination=/admin/structure/views/view/businesses/preview/page_1%3F_wrapper_format%3Ddrupal_ajax";
+    }
+
+    public function getTemplateName()
+    {
+        return "__string_template__249a3a63d9c751b1b9c960794fcedf18";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  39 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "__string_template__249a3a63d9c751b1b9c960794fcedf18", "");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array();
+        static $filters = array();
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                [],
+                [],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
